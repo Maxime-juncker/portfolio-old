fdd1aae200fc8145514f9c440d0dfccaa55dcfbf
Juncker Maxime <82824938+Maxime-juncker@users.noreply.github.com>
2023-12-10 19:18:47 +0100

    finito Maximatron

--- src/Page/Maximatron.jsx
+++ src/Page/Maximatron.jsx
@@ -15,6 +15,8 @@ class Maximatron extends Component {
                 <PointLight left={30} top={60} backgroundColor={"var(--red)"}/>
                 <PointLight left={-20} top={90} backgroundColor={"var(--red)"}/>
                 <PointLight left={50} top={120} backgroundColor={"var(--red)"}/>
+                <PointLight left={30} top={150} backgroundColor={"var(--red)"}/>
+
             </header>
 
             <section className='Content'>
@@ -41,14 +43,27 @@ class Maximatron extends Component {
                             <h2>Platform supported</h2>
                             <div className="Card-conterner">
                                 <SkillCard content={"WINDOWS"} src={require("../Images/Cards/windows.svg").default}/>
-
                             </div>
+
                         </div>
                     </article>
                     <article className='screenshot-contener' style={{backgroundColor: "var(--redLow)"}}> 
                         <img src={require("../Images/Projects/Maximatron/Maximatron-app-screen01.png")} alt="" />
                         <img src={require("../Images/Projects/Maximatron/Maximatron-app-screen02.png")} alt="" />
                     </article>
+                </div>
+                <div className='Features'>
+                <PointLight left={0} top={0} backgroundColor={"var(--red)"}/>
+                <PointLight left={-40} top={170} backgroundColor={"var(--red)"}/>
+
+                    <h2>Features</h2>
+                    <li>Files hierachy</li>
+                    <li>Opening folder / file</li>
+                    <li>Saving / Quick saving file</li>
+                    <li>All action contain shortcut</li>
+                    <li>Custom saving using .maximatron format</li>
+                    <li>App theme match windows accent colors</li>
+                    <li>Lists / note / checkbox blocks</li>
                 </div>
             </section>
             </body>
